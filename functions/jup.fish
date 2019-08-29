280c12fe9fcdb8800287127c329f557376cd95f1
Tony Ibbs <tony.ibbs@treasure-data.com>
2019-08-29 10:47:51 +0100

    Mostly file mode changes

--- functions/jup.fish
+++ functions/jup.fish
@@ -1,4 +1,3 @@
 function jup --description "Run a 'standardised' Jupyter notebook session"
     jupyter notebook --notebook-dir=$HOME/jupyter-notebooks --no-browser --ip=0.0.0.0
 end
-# vim: set tabstop=8 softtabstop=4 shiftwidth=4 expandtab:

769fb96aed1cf61c1cf62f80887612426daff651
Tony Ibbs <tony.ibbs@treasure-data.com>
2020-04-20 11:36:19 +0100

    Prompt changes

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 # Git prompt details. The documentation for this is in the source:
-#   https://github.com/fish-shell/fish-shell/blob/master/share/functions/__fish_git_prompt.fish
+#   https://github.com/fish-shell/fish-shell/blob/master/share/functions/fish_git_prompt.fish
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
@@ -10,10 +10,11 @@ function fish_prompt --description 'Write out the prompt'
     # It would be more elegant to use the fish-defined colour variables,
     # instead of setting explicit colours, but this is simpler, and maps
     # more closely to what I used to do in bash.
-    set_color red
-    echo -n -s "$USER"
+    # set_color red
+    # echo -n -s "$USER"
     set_color normal
-    echo -n -s ' ' (prompt_pwd)
+    # echo -n -s ' ' (prompt_pwd)
+    echo -n -s (prompt_pwd)
     set_color green
     echo -n (__fish_vcs_prompt)
     set_color normal
